680584d6fd24e47bc4f0b483d414e1de1db60f0a
Willian Patrick <superhitec@gmail.com>
2025-02-09 17:59:08 -0300

    fix all

--- obj/Debug/net8.0/Plugfy.Core.Commons.Communication.AssemblyInfo.cs
+++ obj/Debug/net8.0/Plugfy.Core.Commons.Communication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plugfy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb3720b78e0dfdeb99691a565e392bdf38026fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e683cf7c2c92b45116fddbecec536023a69a242")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plugfy.Core.Commons.Communication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plugfy.Core.Commons.Communication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
